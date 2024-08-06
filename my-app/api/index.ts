5ce6a4dd3b92223ef234ccd9b4cf10f1e837f25e
Gledson <gledsoul@gmail.com>
2024-08-06 19:49:21 +0100

    Update index.ts

--- my-app/api/index.ts
+++ my-app/api/index.ts
@@ -1,14 +1,15 @@
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
 
-export const config = {
-  runtime: 'edge'
-}
+export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app.get('/', (c) => {
-  return c.json({ message: 'Hello Hono!' })
+app.get('/hello', (c) => {
+  return c.json({
+    message: 'Hello Next.js!',
+  })
 })
 
-export default handle(app)
+export const GET = handle(app)
+export const POST = handle(app)
